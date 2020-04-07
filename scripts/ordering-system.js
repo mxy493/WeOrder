dd78b07e4cd571606eeac2a61bcd89ff61d534e2
mxy493 <mxy20493@gmail.com>
2020-04-07 15:45:55 +0800

    更新页面切换为iframe框架

--- scripts/ordering-system.js
+++
@@ -1,8 +0,0 @@
-//导航页面切换
-function pageSwitch(id){
-    id.setAttribute("class", "active");
-    var group = id.parentNode.children;
-    for(var i =0,pl= group.length;i<pl;i++) {
-        if(group[i] !== id) group[i].removeAttribute('class');
-    }
-}
