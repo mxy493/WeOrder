a09c0440890fa9bdc18ebe83cf2de98a92fe349d
mxy493 <mxy20493@gmail.com>
2020-04-25 00:09:17 +0800

    修改订单状态页按钮组切换的逻辑代码

--- scripts/order-list.js
+++ scripts/order-list.js
@@ -1,43 +1,20 @@
-//订单状态切换
-function statusChange(id){
-    if(id.disabled == true){
-        return;
-    }
-    var btns = document.getElementById('status-btn-group');
-    var btngroup = btns.children;
-    if(id === btngroup[2]){
-        id.disabled = true;
-    }
-    var cur;
-    for(var i = 0, l = btngroup.length; i < l; i++){
-        if(btngroup[i].className.indexOf("btn-success") >= 0){
-            cur = btngroup[i];
-            break;
+$(document).ready(function(){
+    //实时订单和历史订单切换
+    $(".head-button").on("click", function(){
+        $(this).siblings().attr("class", "btn btn-default head-button");
+        $(this).attr("class", "btn btn-success head-button");
+    })
+
+    //订单状态切换
+    $(".compact-button").on("click", function(){
+        if($(this).hasClass("disabled")){
+            return;
         }
-    }
-    var attribute;
-    switch(cur){
-        case btngroup[0]:
-            if(id===btngroup[1]){
-                attribute = btngroup[0].getAttribute("class");
-                btngroup[0].setAttribute("class", attribute.replace("btn-success", "btn-default"));
-                btngroup[0].disabled = true;
-                
-                attribute = id.getAttribute("class");
-                id.setAttribute("class", attribute.replace("btn-default", "btn-success"));
-            }
-            break;
-        case btngroup[1]:
-            if(id===btngroup[2]){
-                attribute = btngroup[1].getAttribute("class");
-                btngroup[1].setAttribute("class", attribute.replace("btn-success", "btn-default"));
-                btngroup[1].disabled = true;
-                
-                attribute = id.getAttribute("class");
-                id.setAttribute("class", attribute.replace("btn-default", "btn-success"));
-            }
-            break;
-        case btngroup[2]:
-            break;
-    }
-}
+        $(this).prevAll().attr("class", "btn btn-default compact-button");
+        $(this).prevAll().addClass("disabled");
+        $(this).attr("class", "btn btn-success compact-button");
+        if($(this).eq(2)){
+            $(this).addClass("disabled");
+        }
+    })
+})
