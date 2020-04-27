5db1ca07bf1a5c66df60bb3845ff6f0ca5993f30
mxy493 <mxy20493@gmail.com>
2020-04-27 19:12:28 +0800

    更改菜品管理菜品信息修改方式为弹出模态框

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-    $("tbody button").on("click",function(){
-        window.location = "modify-dish.html";
+    $(".table-btn").click(function(){
+        var info = $(this).parent().siblings();
+        $("#dishid").text(info[0].innerText);//菜品ID
+        $("#dishname").val(info[1].innerText);//菜名
+        $("#dishcatg").val(info[2].innerText);//类别
+        $("#setprice").val(info[3].innerText);//单价
+        $("#quantity").val(info[4].innerText);//库存
+        $("#dish_status").val(info[6].innerText);//状态
+        // $("#imgupload").val(info[7].innerText);//图片
+        $("#description").val(info[5].innerText);//描述
+    })
+
+    $(".btn-status").click(function(){
+        $(this).attr("class", "btn btn-success btn-status");
+        $(this).siblings().attr("class", "btn btn-default btn-status");
+    })
+
+    //删除菜品
+    $("#dish_delete").click(function(){
+        //请求服务器删除该菜品
+
+        //服务器删除成功的前提下，从列表中删除该菜品
+        if(rp_status){
+
+        }
+    })
+
+    //修改菜品信息
+    $("dish_modify").click(function(){
+
     })
 })
