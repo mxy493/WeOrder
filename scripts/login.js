6f6b380f98f9e934fc3c605fe660baab79ede19d
mxy493 <mxy20493@gmail.com>
2020-04-30 21:28:34 +0800

    将数据请求函数独立出来供各功能模块调用

--- scripts/login.js
+++ scripts/login.js
@@ -19,114 +19,48 @@ $(document).ready(function(){
 		else if(id != "" && pwd != "")
 		{
 			//身份认证
-			authentication(id, pwd);
+			var data = {
+				"admin_id": id,
+				"admin_password": pwd
+			};
+			XHR(data);//调用XHR函数发起请求
 		}
 	});
+});	
 
-	//请求服务器确认登陆信息
-	function authentication(id, pwd) {
-		// Sending and receiving data in JSON format using POST method
-		//
-		var xhr = new XMLHttpRequest();
-		console.log('UNSENT', xhr.readyState); // readyState 为 0
-		if (!xhr) {
-			alert('Giving up :( Cannot create an XMLHTTP instance');
-			return false;
-		}
 
-		var url = "http://orderingmeal.applinzi.com/merchant/index.php/home/user/denglu";
-		xhr.open("POST", url, true);
-		console.log('OPENED', xhr.readyState); // readyState 为 1
-
-		//设置数据格式
-		xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=utf-8"); 
-
-		xhr.onprogress = function () {
-			console.log('LOADING', xhr.readyState); // readyState 为 3
-		};
-		
-		xhr.onload = function () {
-			console.log('DONE', xhr.readyState); // readyState 为 4
-		};
-
-		var data = {
-			"admin_id": id,
-			"admin_password": pwd
-		};
-
-		console.log(data);
-		xhr.send($.param(data));
-
-		xhr.onreadystatechange = function () {
-			try{
-				console.log("状态改变，当前状态码: "+xhr.readyState);
-				switch(xhr.readyState){
-					case 1:
-						console.log("已调用open()方法.");
-						break;
-					case 2:
-						console.log("send()方法已经被调用, 响应头和响应状态已经返回.");
-						break;
-					case 3:
-						console.log("正在下载响应体.");
-						break;
-					case 4:
-						console.log("传输已结束. 状态码: ", xhr.status);
-						if (xhr.status === 200) {
-							console.log("请求成功，XMLHttpRequest.status: 200");
-							console.log(xhr.responseText);
-							//将返回数据转为JSON对象
-							try{
-								var response = JSON.parse(xhr.responseText);
-								processResponse(response);
-							}
-							catch{
-								console.log("接收的数据无法转为JSON对象.");
-							}
-						}
-						break;
-					default: break;
-				}                
-			}
-			catch(e){
-				alert('Caught Exception: ' + e.description);
-			}
-		}
+//处理返回数据
+function processResponse(response){
+	switch(response.error_code){
+		case 0:
+			window.location = "main-frame.html";
+			break;
+		case 1:
+			alert("参数不足！");
+			break;
+		case 2:
+			$("#login").popover({
+				placement: 'top',
+				title: '登陆失败',
+				content: '不存在该账号!',
+			})
+			$("#login").popover('show');
+			setTimeout(function(){
+				$("#login").popover('hide');
+			}, 2000);
+			break;
+		case 3:
+			$("#login").popover({
+				placement: 'top',
+				title: '登陆失败',
+				content: '密码错误!',
+			})
+			$("#login").popover('show');
+			setTimeout(function(){
+				$("#login").popover('hide');
+			}, 2000);
+			break;
+		default:
+			alert("未知错误！");
 	}
-
-	//处理返回数据
-	function processResponse(response){
-		switch(response.error_code){
-			case 0:
-				window.location = "main-frame.html";
-				break;
-			case 1:
-				alert("参数不足！");
-				break;
-			case 2:
-				$("#login").popover({
-					placement: 'top',
-					title: '登陆失败',
-					content: '不存在该账号!',
-				})
-				$("#login").popover('show');
-				setTimeout(function(){
-					$("#login").popover('hide');
-				}, 2000);
-				break;
-			case 3:
-				$("#login").popover({
-					placement: 'top',
-					title: '登陆失败',
-					content: '密码错误!',
-				})
-				$("#login").popover('show');
-				setTimeout(function(){
-					$("#login").popover('hide');
-				}, 2000);
-				break;
-			default:
-				alert("未知错误！");
-		}
-	}
-});
+}
---
+++ scripts/xhr.js
@@ -0,0 +1,75 @@
+adminInfo = {
+    "id": "admin",
+    "phone": "123456"
+}
+
+//请求服务器确认登陆信息
+function XHR(data) {
+    // Sending and receiving data in JSON format using POST method
+    //
+    var xhr = new XMLHttpRequest();
+    console.log('UNSENT', xhr.readyState); // readyState 为 0
+    if (!xhr) {
+        alert('Giving up :( Cannot create an XMLHTTP instance');
+        return false;
+    }
+
+    var url = "http://orderingmeal.applinzi.com/merchant/index.php/home/user/denglu";
+    xhr.open("POST", url, true);
+    console.log('OPENED', xhr.readyState); // readyState 为 1
+
+    //设置数据格式
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded;charset=utf-8"); 
+
+    xhr.onprogress = function () {
+        console.log('LOADING', xhr.readyState); // readyState 为 3
+    };
+    
+    xhr.onload = function () {
+        console.log('DONE', xhr.readyState); // readyState 为 4
+    };
+
+    console.log(data);
+    var d = $.param(data);
+    xhr.send(d);
+
+    xhr.onreadystatechange = function () {
+        try{
+            console.log("状态改变，当前状态码: "+xhr.readyState);
+            switch(xhr.readyState){
+                case 1:
+                    console.log("已调用open()方法.");
+                    break;
+                case 2:
+                    console.log("send()方法已经被调用, 响应头和响应状态已经返回.");
+                    break;
+                case 3:
+                    console.log("正在下载响应体.");
+                    break;
+                case 4:
+                    console.log("传输已结束. 状态码: ", xhr.status);
+                    if (xhr.status === 200) {
+                        console.log("请求成功，XMLHttpRequest.status: 200");
+                        console.log(xhr.responseText);
+                        //将返回数据转为JSON对象
+                        try{
+                            var response = JSON.parse(xhr.responseText);
+                            processResponse(response);
+                        }
+                        catch{
+                            console.log("接收的数据无法转为JSON对象.");
+                        }
+                    }
+                    break;
+                default: break;
+            }                
+        }
+        catch(e){
+            alert('Caught Exception: ' + e.description);
+        }
+    }
+}
+
+function getAdminInfo(){
+    return adminInfo;
+}
