8ae6a6c627920f633dd4d4676666d8822b8ca1a0
mxy493 <mxy20493@gmail.com>
2020-04-30 16:55:02 +0800

    添加注册成功提示信息，添加账户注册信息的检测机制

--- scripts/register.js
+++ scripts/register.js
@@ -1,20 +1,56 @@
 $(document).ready(function () {
-    //检查信息填写是否符合要求
-    $("#admin_password_again").keyup(function () {
-        var p1 = admin_password.value;
-        var p2 = admin_password_again.value;
-        if (p1 === p2) {
-            $("#admin_password_again").parent().next().text("");
-            $("#submit_button").removeAttr("disabled");
-        }
-        else {
-            $("#admin_password_again").parent().next().text("密码不一致！");
-            $("#submit_button").attr({ "disabled": "true" });
+    //点击提交按钮
+    $("#submit_button").click(function(){
+        if(checkInfo()){
+            //发送数据
+            request();
         }
     })
 
-    //点击提交按钮
-    $("#submit_button").click(function () {
+    //检查信息是否符合要求
+    function checkInfo(){
+        var accord = true;//所有信息是否符合要求
+        if($("#admin_id").val() == "" || $("#admin_name").val() == "" || $("#admin_phone").val() == "" || $("#admin_password").val() == "" || $("#admin_password_again").val() == ""){
+            accord = false;
+            alert("请将信息填写完整！");
+            return;
+        }
+
+        //匹配电话号码
+        // var phone = /^(?:\+?86)?1(?:3\d{3}|5[^4\D]\d{2}|8\d{3}|7(?:[0-35-9]\d{2}|4(?:0\d|1[0-2]|9\d))|9[0-35-9]\d{2}|6[2567]\d{2}|4(?:[14]0\d{3}|[68]\d{4}|[579]\d{2}))\d{6}$/;
+        var phone = /^[1-9][0-9]*/;//匹配任意1-9开头的数字串
+        var ph = $("#admin_phone").val();
+        if(!phone.test(ph)) {
+            $("#admin_phone").parent().next().text("手机号码格式不正确!");
+            accord = false;
+        }
+        else {
+            $("#admin_phone").parent().next().text("");
+        }
+
+        //密码是否一致
+        if (admin_password.value !== admin_password_again.value) {
+            accord = false;
+        }
+
+        return accord;
+    }
+
+    //检查密码是否一致
+    $("#admin_password_again").keyup(function(){
+        if (admin_password.value === admin_password_again.value) {
+            $("#admin_password_again").parent().next().text("");
+            // $("#submit_button").removeAttr("disabled");
+        }
+        else {
+            accord = false;
+            $("#admin_password_again").parent().next().text("密码不一致！");
+            // $("#submit_button").attr({ "disabled": "true" });
+        }
+    })
+    
+    //发起请求
+    function request() {
         // Sending and receiving data in JSON format using POST method
         //
         var xhr = new XMLHttpRequest();
@@ -71,7 +107,7 @@ $(document).ready(function () {
                             //将返回数据转为JSON对象
                             try{
                                 var response = JSON.parse(xhr.responseText);
-
+                                processResponse(response);
                             }
                             catch{
                                 console.log("接收的数据无法转为JSON对象.");
@@ -85,7 +121,7 @@ $(document).ready(function () {
                 alert('Caught Exception: ' + e.description);
             }
         };
-    })
+    }
 
     //处理收到的数据
     function processResponse(response){
@@ -93,7 +129,18 @@ $(document).ready(function () {
             case 0:
                 console.log("注册成功！");
                 //跳转到系统后台页面，思路：带用户ID跳转以验证用户身份
-                window.location = "index.html";
+                //延迟2秒跳转登陆页面
+                $("#submit_button").popover({
+                    placement: 'top',
+                    title: '注册成功',
+                    content: '随后将跳转到登陆页面!',
+                    triger: 'click'
+                })
+                $("#submit_button").popover('show');
+                //延迟2秒跳转到添加菜品页面
+                setTimeout(function() { 
+                    window.location = "index.html";
+                }, 2000);
                 break;
             case 1:
                 console.log("参数不足！");
