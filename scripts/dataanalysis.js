04ae5e2dcc82bc25b1cdeb7efa86a9b885cd7bbc
mxy493 <mxy20493@gmail.com>
2020-06-09 16:37:49 +0800

    完善数据分析近7日收入折线图

--- scripts/dataanalysis.js
+++ scripts/dataanalysis.js
@@ -4,32 +4,67 @@ $(document).ready(function () {
         window.parent.location.replace("index.html");
     }
 
-    data_daily_income();
-    data_daily_sales();
-
+    getData();
 });
 
-function data_daily_income() {
+function getData() {
+    //近7日销售额
+    var url = "http://orderingmeal.applinzi.com/merchant/index.php/home/view/weeksale";
+    var data = {};
+    XHR(url, data);
+
+    //其它
+    data_daily_sales();
+}
+
+function data_daily_income(response_data) { 
     // 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('daily_income'));
+    console.log(response_data);
+    
+    //数据格式化
+    date = [];
+    income = [];
+    for (var x in response_data){
+        // 格式为"2020-06-03"
+        split_x = x.split("-");
+        date.push(parseInt(split_x[1]).toString() + '.' + parseInt(split_x[2]).toString());
+        income.push(response_data[x]);
+    }
 
     // 指定图表的配置项和数据
     var option = {
         title: {
-            text: '日收入'
+            show: true,
+            text: '最近收入',
         },
         tooltip: {},
         legend: {
             data: ['收入'],
         },
         xAxis: {
-            data: ["3.21", "3.22", "3.23", "3.24", "3.25", "3.26", "3.27"]
+            name: '日期',
+            // data: ["3.21", "3.22", "3.23", "3.24", "3.25", "3.26", "3.27"]
+            data: date.reverse()
         },
-        yAxis: {},
+        yAxis: {
+            name: '金额'
+        },
+        grid: {
+            show: false
+        },
         series: [{
-            name: '日收入',
+            name: '日收入（元）',
             type: 'line',
-            data: [100, 200, 360, 240, 150, 280, 220],
+            // data: [100, 200, 360, 240, 150, 280, 220],
+            data: income.reverse(),
+            symbol: 'emptyCircle',
+            symbolSize: 8,
+            lineStyle: {
+                color: "#3fb1e3",
+                width: 4
+            },
+            smooth: false  // 平滑
         }]
     };
     // 使用刚指定的配置项和数据显示图表。
@@ -49,16 +84,32 @@ function data_daily_sales() {
         legend: {
             data: ['销量']
         },
-        xAxis: {
-            data: ["白菜", "萝卜", "莲藕", "牛肉", "猪肉", "莴苣", "生菜"]
-        },
-        yAxis: {},
         series: [{
             name: '日销量',
-            type: 'bar',
-            data: [10, 20, 30, 24, 15, 28, 22],
+            type: 'pie',
+            data: [
+                {name: "白菜", value: 2},
+                {name: "萝卜", value: 20},
+                {name: "莲藕", value: 30},
+                {name: "牛肉", value: 24},
+                {name: "猪肉", value: 15},
+            ],
+            minAngle: 30
         }]
     };
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
+}
+
+//处理返回数据
+function processResponse(response){
+    //这是一个订单获取的请求
+    if(response.function == "weeksale" && response.request_behave == "select"){
+        if(response.error_code == 0){
+            data_daily_income(response.data);
+        }
+        else {
+            alert("近7日收入数据获取失败！");
+        }
+    }
 }
